154726a475b23262001df18d463e5772e7798b18
Jonathan Reis <jonathan.reis@estudante.ufjf.br>
2023-11-22 20:58:28 -0300

    insere funcoes setJoao e setMaria

--- src/joaoEMaria.js
+++ src/joaoEMaria.js
@@ -1,6 +1,22 @@
 let joao = 1;
 let maria = 2;
 
+function setJoao(valor){
+    if(valor >= 0){
+        joao = valor;
+    } else {
+        joao = 0;
+    }
+}
+
+function setMaria(valor){
+    if(valor>=0){
+        maria = valor;
+    } else {
+        maria = 0;
+    }
+}
+
 function getMaria(){
     return maria;
 }
@@ -9,4 +25,12 @@ function getJoao(){
     return joao;
 }
 
-export {getMaria, getJoao};
+function deMariaParaJoao(){
+    joao = joao + maria;
+    maria = 0;
+}
+function deJoaoParaMaria() {
+    maria = maria + joao;
+    joao = 0;
+}
+export {getJoao, getMaria,deJoaoParaMaria, deMariaParaJoao, setJoao, setMaria};
